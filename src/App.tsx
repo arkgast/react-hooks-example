d5ce17efb209674e8105f938919c5ef6e5894873
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-04-16 16:13:56 -0500

    feat: add todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
+const initialTodos = [
+  {
+    id: 'a',
+    task: 'Learn Node.js',
+    complete: true
+  }, {
+    id: 'b',
+    task: 'Learn Firebase',
+    complete: true
+  }, {
+    id: 'c',
+    task: 'Learn React-Hooks',
+    complete: false
+  }
+]
+
 class App extends Component {
   render() {
     return (
-      <h1>Hooks Example</h1>
+      <div>
+        <ul>
+          {initialTodos.map(todo => (
+            <li key={todo.id}>
+              <label>{todo.task}</label>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
