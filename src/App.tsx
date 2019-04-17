a4436af0195e2a8d113a10095670991e8b157f6c
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-04-17 10:18:18 -0500

    feat: implement form to aggregate todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v4'
 
 const initialTodos = [
   {
-    id: 'a',
+    id: uuid(),
     task: 'Learn Node.js',
     complete: true
   }, {
-    id: 'b',
+    id: uuid(),
     task: 'Learn Firebase',
     complete: true
   }, {
-    id: 'c',
+    id: uuid(),
     task: 'Learn React-Hooks',
     complete: false
   }
 ]
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          {initialTodos.map(todo => (
-            <li key={todo.id}>
-              <label>{todo.task}</label>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+const App = () => {
+  const [todos, setTodo] = useState(initialTodos)
+  const [task, setTask] = useState('')
+
+  const handleSubmit = (event: React.FormEvent) => {
+    if (task) {
+      const todo = {
+        id: uuid(),
+        task,
+        complete: false
+      }
+      setTodo(todos.concat(todo))
+    }
+    setTask('')
+    event.preventDefault()
   }
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value)
+  }
+
+  return (
+    <div>
+      <ul>
+        {todos.map(todo => (
+        <li key={todo.id}>
+          <label>{todo.task}</label>
+        </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input value={task} onChange={handleChangeInput} />
+      </form>
+    </div>
+  );
 }
 
 export default App;
